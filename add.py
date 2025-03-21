eb4072c2ae469da1337fb7bbbcd2227cf1245b3f
abhiral pradhan <111888167+A1abhiral@users.noreply.github.com>
2025-03-21 10:50:16 +0545

    Added addition operation

--- add.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Adding the numbers
-sum_result = num1 + num2
-
-# Displaying the result
-print(f"The sum of {num1} and {num2} is {sum_result}")
